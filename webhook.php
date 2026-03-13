fe306ff842ab53a63fec4b872d30243aa42db7b2
Marcelob123 <54921539+Marcelob123@users.noreply.github.com>
2026-03-12 21:24:02 -0300

    Versão 1

--- webhook.php
+++ webhook.php
@@ -1,6 +1,6 @@
 <?php
 $data = file_get_contents("php://input");
-file_put_contents("github.txt", $data.PHP_EOL, FILE_APPEND);
+file_put_contents("github-log.txt", $data.PHP_EOL, FILE_APPEND);
 http_response_code(200);
 echo "OK";
 ?>
